2317fa8806174bff15a3da91ad5e2c412b814c17
Sandeep <116249507+sandeep2351@users.noreply.github.com>
2024-08-18 15:49:40 +0530

    Update ApiPath.js

--- src/Vendordashboard/helpers/ApiPath.js
+++ src/Vendordashboard/helpers/ApiPath.js
@@ -1,4 +1,4 @@
-// ApiPath.js
-export const API_URL = 'http://localhost:9090';
+// // ApiPath.js
+// export const API_URL = 'http://localhost:9090';
 
-//export const API_URL="https://Backend_Nodejs_swiggy.onrender.com"
+export const API_URL="https://backend-nodejs-swiggy-3lmx.onrender.com"
